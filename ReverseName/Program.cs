718d917aa95757ea5002794f62babf191cbf1466
Debashis <jupiter_104@ymail.com>
2019-03-29 19:51:05 +0530

    Add files via upload

---
+++ ReverseName/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReverseName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string input = Console.ReadLine();
+
+            string reversedName = "";
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reversedName += input[i];
+            }
+
+            Console.WriteLine("After reverse: " + reversedName);
+            Console.ReadLine();
+        }
+    }
+}
